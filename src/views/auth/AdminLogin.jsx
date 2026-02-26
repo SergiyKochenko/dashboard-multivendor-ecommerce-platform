064344d24c8f9b5fceaf4e71420618e4f51c71ec
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-02-26 12:32:59 +0000

    Added Error And Notification Messages

--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react'; 
-import { useDispatch } from 'react-redux';
-import { admin_login } from '../../store/Reducers/authReducer';
+import React, { useEffect, useState } from 'react'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { admin_login,messageClear } from '../../store/Reducers/authReducer';
+import { PropagateLoader } from 'react-spinners';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const AdminLogin = () => {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate();
+    const {loader,errorMessage, successMessage} = useSelector(state=>state.auth)
 
     const [state, setState] = useState({ 
         email: "",
@@ -24,6 +29,26 @@ const AdminLogin = () => {
         // console.log(state)
     }
 
+    const overrideStyle = {
+            display: "flex",
+            margin: "0 auto",
+            height: '24px',
+            justifyContent: "center",
+            alignItems: "center",
+        };
+
+        useEffect(() => {
+        if (errorMessage) {
+            toast.error(errorMessage)
+            dispatch(messageClear())
+        }
+    if (successMessage) {
+            toast.success(successMessage)
+            dispatch(messageClear())  
+            navigate('/')          
+        }
+    },[errorMessage, successMessage, dispatch, navigate])
+
     return (
         <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center' >
           <div className='w-[350px] text-[#ffffff] p-2'>
@@ -31,7 +56,7 @@ const AdminLogin = () => {
                
         <div className='h-[70px] flex justify-center items-center'>
             <div className='w-[180px] h-[50px]'>
-                <img className='w-full h-full' src="http://localhost:3000/images/logo.png" alt="image" />
+                <img className='w-full h-full' src="http://localhost:3000/images/logo.png" alt="logo" />
             </div>
             </div>       
 
@@ -49,7 +74,11 @@ const AdminLogin = () => {
         </div>
  
 
-        <button className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Login</button>
+        <button disabled={loader ? true : false}  className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>
+            {
+               loader ? <PropagateLoader color='#fff' cssOverride={overrideStyle} /> : 'Login'
+            } 
+            </button>
  
     </form>
  
