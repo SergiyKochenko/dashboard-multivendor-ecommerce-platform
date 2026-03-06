7aecd2b841ff42ac35e27197b5899ac5b533c365
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-06 16:05:51 +0000

    Setup Category Add & Get

--- src/store/Reducers/categoryReducer.js
+++ src/store/Reducers/categoryReducer.js
@@ -10,7 +10,7 @@ export const categoryAdd = createAsyncThunk(
             formData.append('name', name)
             formData.append('image', image)
             const {data} = await api.post('/category-add',formData,{withCredentials: true}) 
-            console.log(data)
+            // console.log(data)
             return fulfillWithValue(data)
         } catch (error) {
             // console.log(error.response.data)
@@ -19,6 +19,25 @@ export const categoryAdd = createAsyncThunk(
     }
 )
 
+// End Method 
+
+export const get_category = createAsyncThunk(
+    'category/get_category',
+    async({ parPage,page,searchValue },{rejectWithValue, fulfillWithValue}) => {
+        
+        try {
+             
+            const {data} = await api.get(`/category-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`,{withCredentials: true}) 
+            // console.log(data)
+            return fulfillWithValue(data)
+        } catch (error) {
+            // console.log(error.response.data)
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
+
+  // End Method
   
  
 export const categoryReducer = createSlice({
@@ -27,7 +46,8 @@ export const categoryReducer = createSlice({
         successMessage :  '',
         errorMessage : '',
         loader: false,
-        categorys : [] 
+        categorys : [], 
+        totalCategory: 0 
     },
     reducers : {
 
@@ -45,12 +65,18 @@ export const categoryReducer = createSlice({
             state.loader = false;
             state.errorMessage = payload.error
         })  
-        // .addCase(admin_login.fulfilled, (state, { payload }) => {
-        //     state.loader = false;
-        //     state.successMessage = payload.message
-        //     state.token = payload.token
-        //     state.role = returnRole(payload.token)
-        // })
+        .addCase(categoryAdd.fulfilled, (state, { payload }) => {
+            state.loader = false;
+            state.successMessage = payload.message
+            state.categorys = [...state.categorys, payload.category]
+             
+        })
+
+        .addCase(get_category.fulfilled, (state, { payload }) => {
+            state.totalCategory = payload.totalCategory;
+            state.categorys = payload.categorys;
+             
+        })
  
 
     }
--- src/views/admin/Category.jsx
+++ src/views/admin/Category.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; 
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Pagination from '../Pagination';
 import { FaE } from 'react-icons/fa6';
@@ -7,13 +7,15 @@ import { FaImage } from "react-icons/fa";
 import { IoMdCloseCircle } from "react-icons/io";
 import { PropagateLoader } from 'react-spinners';
 import { overrideStyle } from '../../utils/utils';
-import { categoryAdd } from '../../store/Reducers/categoryReducer';
+import { categoryAdd, messageClear,get_category } from '../../store/Reducers/categoryReducer';
 import { useDispatch, useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
+import Search from '../components/Search';
 
 const Category = () => {
 
     const dispatch = useDispatch()
-    const {loader} = useSelector(state=> state.category)
+    const {loader,successMessage,errorMessage,categorys} = useSelector(state=> state.category)
 
 
     const [currentPage, setCurrentPage] = useState(1)
@@ -47,6 +49,36 @@ const Category = () => {
         // console.log(state)
     }
 
+    useEffect(() => {
+
+        if (successMessage) {
+            toast.success(successMessage)
+            dispatch(messageClear()) 
+            setState({
+                name: '',
+                image: ''
+            }) 
+            setImage('')
+
+        }
+        if (errorMessage) {
+            toast.error(errorMessage)
+            dispatch(messageClear())
+        }
+        
+
+    },[successMessage, errorMessage])
+
+    useEffect(() => {
+        const obj = {
+            parPage: parseInt(parPage),
+            page: parseInt(currentPage),
+            searchValue
+        }
+        dispatch(get_category(obj))
+
+    },[searchValue, currentPage,parPage])
+
 
     return (
         <div className='px-2 lg:px-7 pt-5'>
@@ -62,14 +94,7 @@ const Category = () => {
                 <div className='w-full lg:w-7/12'>
                 <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
 
-                <div className='flex justify-between items-center'>
-                    <select onChange={(e) => setParPage(parseInt(e.target.value))} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'>
-                        <option value="5">5</option>
-                        <option value="10">10</option>
-                        <option value="20">20</option> 
-                    </select>
-                    <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="text" placeholder='search' /> 
-                </div>
+            <Search setParPage={setParPage} setSearchValue={setSearchValue} searchValue={searchValue}  />
 
    <div className='relative overflow-x-auto'>
     <table className='w-full text-sm text-left text-[#d0d2d6]'>
@@ -84,12 +109,12 @@ const Category = () => {
 
         <tbody>
             {
-                [1,2,3,4,5].map((d, i) => <tr key={i}>
-                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{d}</td>
+                categorys.map((d, i) => <tr key={i}>
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{i+1}</td>
                 <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                    <img className='w-[45px] h-[45px]' src={`http://localhost:3000/images/category/${d}.jpg`} alt="" />
+                    <img className='w-[45px] h-[45px]' src={d.image} alt="" />
                 </td>
-                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Tshirt</td>
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{d.name}</td>
                 <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
                     <div className='flex justify-start items-center gap-4'>
                     <Link className='p-[6px] bg-yellow-500 rounded hover:shadow-lg hover:shadow-yellow-500/50'> <FaEdit/> </Link> 
