eb52d11e5ca8a3b33d0b58d59f315e2c9419a82c
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-08 21:54:02 +0000

    All .js, .jsx, .json, and .md files in  dashboard project have been auto-formatted with Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
-import { useEffect, useState } from "react";
-import Router from "./router/Router";
-import publicRoutes from "./router/routes/publicRoutes";
-import { getRoutes } from "./router/routes";
-import { useDispatch, useSelector } from "react-redux";
-import { get_user_info } from "./store/Reducers/authReducer";
+import { useEffect, useState } from 'react';
+import Router from './router/Router';
+import publicRoutes from './router/routes/publicRoutes';
+import { getRoutes } from './router/routes';
+import { useDispatch, useSelector } from 'react-redux';
+import { get_user_info } from './store/Reducers/authReducer';
 
 function App() {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
 
-    const dispatch = useDispatch()
-    const { token } = useSelector(state => state.auth)
+  const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
+  // console.log(allRoutes)
 
-    const [allRoutes, setAllRoutes] = useState([...publicRoutes])
-    // console.log(allRoutes)
+  useEffect(() => {
+    const routes = getRoutes();
+    setAllRoutes([...allRoutes, routes]);
+  }, []);
 
-    useEffect(() => {
-        const routes = getRoutes()
-        setAllRoutes([...allRoutes,routes])
-    },[])
+  useEffect(() => {
+    if (token) {
+      dispatch(get_user_info());
+    }
+  }, [token]);
 
-    useEffect(() => {
-        if (token) {
-            dispatch(get_user_info())
-        }
-
-    },[token])
-
-
-    return <Router allRoutes={allRoutes} /> 
+  return <Router allRoutes={allRoutes} />;
 }
 
 export default App;
--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -3,15 +3,13 @@ import MainLayout from './../../layout/MainLayout';
 import ProtectRoute from './ProtectRoute';
 
 export const getRoutes = () => {
+  privateRoutes.map((r) => {
+    r.element = <ProtectRoute route={r}>{r.element}</ProtectRoute>;
+  });
 
-    privateRoutes.map(r => {
-        
-       r.element = <ProtectRoute route={r} >{r.element}</ProtectRoute>
-    })
-
-    return {
-        path : '/',
-        element : <MainLayout />,
-        children : privateRoutes
-    }
-}
+  return {
+    path: '/',
+    element: <MainLayout />,
+    children: privateRoutes,
+  };
+};
