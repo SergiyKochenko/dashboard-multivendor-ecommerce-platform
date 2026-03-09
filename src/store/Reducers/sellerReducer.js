2abe343c9d1fb938dd9dd91c491fe4d16e8650de
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-09 15:27:12 +0000

    Seller Details And Status Management

--- src/store/Reducers/sellerReducer.js
+++ src/store/Reducers/sellerReducer.js
@@ -20,6 +20,38 @@ export const get_seller_request = createAsyncThunk(
 
 // End Method
 
+export const get_seller = createAsyncThunk(
+  'seller/get_seller',
+  async (sellerId, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.get(`/get-seller/${sellerId}`, { withCredentials: true });
+      console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      // console.log(error.response.data)
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// End Method
+
+export const seller_status_update = createAsyncThunk(
+  'seller/seller_status_update',
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.post(`/seller-status-update`, info, { withCredentials: true });
+      console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      // console.log(error.response.data)
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// End Method
+
 export const sellerReducer = createSlice({
   name: 'seller',
   initialState: {
@@ -28,6 +60,7 @@ export const sellerReducer = createSlice({
     loader: false,
     sellers: [],
     totalSeller: 0,
+    seller: '',
   },
   reducers: {
     messageClear: (state, _) => {
@@ -35,10 +68,19 @@ export const sellerReducer = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(get_seller_request.fulfilled, (state, { payload }) => {
-      state.sellers = payload.sellers;
-      state.totalSeller = payload.totalSeller;
-    });
+    builder
+
+      .addCase(get_seller_request.fulfilled, (state, { payload }) => {
+        state.sellers = payload.sellers;
+        state.totalSeller = payload.totalSeller;
+      })
+      .addCase(get_seller.fulfilled, (state, { payload }) => {
+        state.seller = payload.seller;
+      })
+      .addCase(seller_status_update.fulfilled, (state, { payload }) => {
+        state.seller = payload.seller;
+        state.successMessage = payload.message;
+      });
   },
 });
 export const { messageClear } = sellerReducer.actions;
