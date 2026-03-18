7b150bdf3b35e1d8c5e131bc27624259126575fe
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-18 14:00:04 +0000

    Seller to Customer Chat

--- src/views/admin/Category.jsx
+++ src/views/admin/Category.jsx
@@ -207,7 +207,7 @@ const Category = () => {
                   <div className="mt-4">
                     <button
                       disabled={loader ? true : false}
-                      className="bg-red-800 w-full hover:shadow-red-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3"
+                      className="bg-red-500 w-full hover:shadow-red-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3"
                     >
                       {loader ? (
                         <PropagateLoader color="#fff" cssOverride={overrideStyle} />
