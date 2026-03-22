768d3f54bcddfee0db4bee359bbfec2ee9192bb1
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-22 19:33:21 +0000

    Get Orders Showup

--- src/store/rootReducers.js
+++ src/store/rootReducers.js
@@ -1,3 +1,4 @@
+import OrderReducer from './Reducers/OrderReducer';
 import authReducer from './Reducers/authReducer';
 import categoryReducer from './Reducers/categoryReducer';
 import chatReducer from './Reducers/chatReducer';
@@ -8,8 +9,8 @@ const rootReducer = {
   auth: authReducer,
   category: categoryReducer,
   product: productReducer,
-
   seller: sellerReducer,
   chat: chatReducer,
+  order: OrderReducer,
 };
 export default rootReducer;
