9bbe3df43bc787f1c63b4ae16ee40cf339390f35
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-22 18:02:16 +0000

    Activate and Deactivate Sellers

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -117,7 +117,7 @@ export const logout = createAsyncThunk(
   async ({ navigate, role }, { rejectWithValue, fulfillWithValue }) => {
     try {
       const { data } = await api.get('/logout', { withCredentials: true });
-      localStorage.removeItem('accessToken', data.token);
+      localStorage.removeItem('accessToken');
       if (role === 'admin') {
         navigate('/admin/login');
       } else {
