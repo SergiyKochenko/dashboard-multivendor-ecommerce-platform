f6e541df9b6ba044584d6e39e75de3a9378505f8
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-23 11:05:03 +0000

    Admin Order Detailes

--- src/store/Reducers/OrderReducer.js
+++ src/store/Reducers/OrderReducer.js
@@ -18,6 +18,34 @@ export const get_admin_orders = createAsyncThunk(
 
 // End Method
 
+export const get_admin_order = createAsyncThunk(
+  'orders/get_admin_order',
+  async (orderId, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.get(`/admin/order/${orderId}`, { withCredentials: true });
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+// End Method
+
+export const admin_order_status_update = createAsyncThunk(
+  'orders/admin_order_status_update',
+  async ({ orderId, info }, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.put(`/admin/order-status/update/${orderId}`, info, {
+        withCredentials: true,
+      });
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+// End Method
+
 export const OrderReducer = createSlice({
   name: 'order',
   initialState: {
@@ -34,10 +62,21 @@ export const OrderReducer = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(get_admin_orders.fulfilled, (state, { payload }) => {
-      state.myOrders = payload.orders;
-      state.totalOrder = payload.totalOrder;
-    });
+    builder
+
+      .addCase(get_admin_orders.fulfilled, (state, { payload }) => {
+        state.myOrders = payload.orders;
+        state.totalOrder = payload.totalOrder;
+      })
+      .addCase(get_admin_order.fulfilled, (state, { payload }) => {
+        state.order = payload.order;
+      })
+      .addCase(admin_order_status_update.rejected, (state, { payload }) => {
+        state.errorMessage = payload.message;
+      })
+      .addCase(admin_order_status_update.fulfilled, (state, { payload }) => {
+        state.successMessage = payload.message;
+      });
   },
 });
 export const { messageClear } = OrderReducer.actions;
